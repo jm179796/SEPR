04779adf0cbdde34dd06b3282d5fa9b6df40eee2
NotKieran <kjh532@york.ac.uk>
2016-11-11 12:18:40 +0000

    Implemented Unit test

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
-@Test
-public class Test {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
+
+public class Test {
+	@Test
     public static void main(String[] args) {
         assertThat((1+1), is(2));
     }
