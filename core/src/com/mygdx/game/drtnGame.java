155ac90808a57a8bac806465b7670625cb9e0797
NotKieran <kjh532@york.ac.uk>
2016-11-16 11:31:25 +0000

    Updated gradle build files

--- core/src/com/mygdx/game/drtnGame.java
+++ core/src/com/mygdx/game/drtnGame.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class DRTN_Game extends ApplicationAdapter {
+public class drtnGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
