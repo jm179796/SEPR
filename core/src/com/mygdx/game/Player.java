c5eaf1194431aa6da850015455d287212c549920
jm179796 <jm1797@york.ac.uk>
2016-11-18 18:23:41 +0000

    Update Player.java

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -52,8 +52,8 @@ public class Player {
   public void changeMoney(int cost){
       this.Money += cost;
   }
-  public void varyResource(string resource, int amount){
+  //public void varyResource(string resource, int amount){
       //Player.resource += amount;
-  }
+  //}
 
 }
