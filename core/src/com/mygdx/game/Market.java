3fc86fc2689127b2c94389e0ca0ffd100a36ed04
Martynas <mm1544@york.ac.uk>
2016-11-21 21:06:39 +0000

    Improved Buy()
    Improved Buy()

--- core/src/com/mygdx/game/Market.java
+++ core/src/com/mygdx/game/Market.java
@@ -1,4 +1,7 @@
 package com.mygdx.game;
+
+import java.util.*;
+
 public class Market {
 
   private Integer OreStock = 0;
@@ -25,23 +28,47 @@ public class Market {
 
   public void buy(String Stock_Type, int Quantity ) {
     switch(Stock_Type){
-      case "ore": if(Quantity < OreStock) {
-        OreStock = OreStock - Quantity;
-        // need to change players stock value and money
+      case "ore":
+          if(Quantity < OreStock) {
+            int OrePrice;
+            OrePrice = OreBuyPrice * Quantity;
+            if(Player.Money >= OrePrice){
+                OreStock -= Quantity;
+                Player.Money -= OrePrice;
+                Player.OreCount += Quantity;
+            } else {
+                System.out.println("Insufficient money");
+                }
+          } else {
+                  System.out.println("Inscufficient resources");
+                }
+      case "food":
+          if (Quantity < FoodStock) {
+              int FoodPrice;
+              FoodPrice = FoodBuyPrice * Quantity;
+              if(Player.Money >= FoodPrice){
+                  FoodStock -= Quantity;
+                  Player.Money -= FoodPrice;
+                  Player.FoodCount += Quantity;
+              } else {
+                  System.out.println("Insufficient money");
+              }
 
-                } else {
+          } else {
                   System.out.println("Inscufficient resources");
                 }
-      case "food": if (Quantity < FoodStock) {
-        FoodStock = FoodStock - Quantity;
-        // need to change players stock value and money
+      case "energy":
+          if (Quantity < EnergyStock) {
+                int EnergyPrice;
+                EnergyPrice = EnergyBuyPrice * Quantity;
+                if(Player.Money >= EnergyPrice){
+                   EnergyStock -= Quantity;
+                   Player.Money -= EnergyPrice;
+                   Player.EnergyCount += Quantity;
                 } else {
-                  System.out.println("Inscufficient resources");
+                    System.out.println("Insufficient money");
                 }
-      case "energy": if (Quantity < EnergyStock) {
-        EnergyStock = EnergyStock - Quantity;
-        // need to change players stock value and money
-      } else {
+          } else {
         System.out.println("Inscufficient resources");
       }
 
@@ -50,7 +77,10 @@ public class Market {
   public void sell(String Stock_Type, int Quantity) {
         switch(Stock_Type){
             case "ore":
-                OreStock = OreStock + Quantity;
+                if(Player.OreCount >= Quantity){
+                    OreStock = OreStock + Quantity;
+                }
+
                 // need to change players stock value and money
 
             case "food": FoodStock = FoodStock + Quantity;
