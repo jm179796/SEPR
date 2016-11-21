3d3b209ebaa4fb5320d272f200b4f35a05f56d59
Martynas <mm1544@york.ac.uk>
2016-11-21 19:11:37 +0000

    Buy and Sell(not none)
    Buy and sell methods not fully done

--- core/src/com/mygdx/game/Market.java
+++ core/src/com/mygdx/game/Market.java
@@ -24,16 +24,50 @@ public class Market {
   private Integer RoboticonBuyPrice;
 
   public void buy(String Stock_Type, int Quantity ) {
+    switch(Stock_Type){
+      case "ore": if(Quantity < OreStock) {
+        OreStock = OreStock - Quantity;
+        // need to change players stock value and money
+
+                } else {
+                  System.out.println("Inscufficient resources");
+                }
+      case "food": if (Quantity < FoodStock) {
+        FoodStock = FoodStock - Quantity;
+        // need to change players stock value and money
+                } else {
+                  System.out.println("Inscufficient resources");
+                }
+      case "energy": if (Quantity < EnergyStock) {
+        EnergyStock = EnergyStock - Quantity;
+        // need to change players stock value and money
+      } else {
+        System.out.println("Inscufficient resources");
+      }
 
   }
 
   public void sell(String Stock_Type, int Quantity) {
+        switch(Stock_Type){
+            case "ore":
+                OreStock = OreStock + Quantity;
+                // need to change players stock value and money
+
+            case "food": FoodStock = FoodStock + Quantity;
+                // need to change players stock value and money
+
+            case "energy":
+                EnergyStock = EnergyStock + Quantity;
+                // need to change players stock value and money
+
+    }
   }
 
   public void gamble() {
   }
 
   public void calculateNewCost() {
+
   }
 
   //public Integer getPrice(String Stock_Type){
