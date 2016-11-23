10d3c715fbd9036e6de8facb5487acdc446e0abd
MisterSeph <josephleakey@gmail.com>
2016-11-23 13:20:28 +0000

    Test Button now renders and accepts user input

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 /**
  * Created by Joseph on 21/11/2016.
@@ -33,22 +34,30 @@ public class MainMenu implements Screen {
         table = new Table();
         //Initialise stage and button-table
 
+        Gdx.input.setInputProcessor(stage);
+        //Set the stage up to access user inputs
+
         table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         //Fill the screen with the table
         //This is bound to change in the future for obvious reasons
 
         TextButton.TextButtonStyle menuButtonStyle = new TextButton.TextButtonStyle();
-        menuButtonStyle.font = new BitmapFont(Gdx.files.internal("font/testfontbignoodle.ttf"), false);
+        menuButtonStyle.font = new BitmapFont(Gdx.files.internal("font/testfont.fnt"), false);
+        menuButtonStyle.pressedOffsetX = 1;
+        menuButtonStyle.pressedOffsetY = -1;
         //Set up the format for the buttons on the menu
-        //NEED TO CONFIGURE FONT
+        //FONTS NEED TO BE STORED AS .FNT FILES
+        //STILL NEED TO SORT OUT BUTTON ANIMATIONS
 
-        testButton = new TextButton("TEST", menuButtonStyle);
+        testButton = new TextButton("TEST BUTTON", menuButtonStyle);
+        testButton.pad(20);
         //Initialise test button using defined style
 
         table.add(testButton);
         table.debug();
         stage.addActor(table);
         //Add test button to table on the interface
+        //"Debug" instruction renders temporary border around button
     }
 
     @Override
