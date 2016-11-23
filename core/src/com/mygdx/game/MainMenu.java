16f720a6d42d2d1e3d937531fa0969746887263b
MisterSeph <josephleakey@gmail.com>
2016-11-23 13:57:35 +0000

    Pushing current build for Nico to work from
    Very close to implementing TTF support

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -3,12 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 /**
  * Created by Joseph on 21/11/2016.
@@ -34,15 +36,25 @@ public class MainMenu implements Screen {
         table = new Table();
         //Initialise stage and button-table
 
+        FreeTypeFontGenerator TTFGenerator;
+        FreeTypeFontGenerator.FreeTypeFontParameter TTFStyle = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        //Set up a font-generator and a style configuration for it to work from
+        //This will serve to convert vectorial .TTF fonts into bitmapped .FNT fonts on the fly
+        //Just instantiate TTFGenerator with a reference to a .TTF file...
+        //...change the settings in TTFStyle...
+
         Gdx.input.setInputProcessor(stage);
         //Set the stage up to access user inputs
+
+        BitmapFont testFont = new BitmapFont(Gdx.files.internal("font/testfont.fnt"));
+        //Set up font for easy access later on
 
         table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         //Fill the screen with the table
         //This is bound to change in the future for obvious reasons
 
         TextButton.TextButtonStyle menuButtonStyle = new TextButton.TextButtonStyle();
-        menuButtonStyle.font = new BitmapFont(Gdx.files.internal("font/testfont.fnt"), false);
+        menuButtonStyle.font = testFont;
         menuButtonStyle.pressedOffsetX = 1;
         menuButtonStyle.pressedOffsetY = -1;
         //Set up the format for the buttons on the menu
@@ -53,11 +65,19 @@ public class MainMenu implements Screen {
         testButton.pad(20);
         //Initialise test button using defined style
 
+        //ROW 1
+        Label title = new Label("Duck-Related Game", new Label.LabelStyle(testFont, Color.BLACK));
+        title.setFontScale(2);
+        table.add(title);
+
+        //ROW 2
+        table.row();
         table.add(testButton);
+
+        //FINALISE TABLE
         table.debug();
         stage.addActor(table);
-        //Add test button to table on the interface
-        //"Debug" instruction renders temporary border around button
+        //"Debug" instruction renders temporary borders around elements
     }
 
     @Override
@@ -65,6 +85,7 @@ public class MainMenu implements Screen {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         //OpenGL nonsense
+        //First instruction sets background colour
 
         stage.act(delta);
         stage.draw();
