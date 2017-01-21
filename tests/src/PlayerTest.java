4e5d2be9bf33c7f029970e05c6042bdc7a86957f
NotKieran <kjh532@york.ac.uk>
2017-01-21 15:47:28 +0000

    Started Player Unit tests

--- tests/src/PlayerTest.java
+++ tests/src/PlayerTest.java
@@ -1,10 +1,11 @@
 import com.badlogic.gdx.Game;
-import com.mygdx.game.Main;
-import com.mygdx.game.Player;
-import com.mygdx.game.Roboticon;
-import com.mygdx.game.Tile;
+import com.mygdx.game.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 /**
  * @author Nico Pinedo NWP503
  * @version 1.0
@@ -13,7 +14,7 @@ import org.junit.Test;
 public class PlayerTest extends TesterFile {
 
     private Game game = new Main();
-    private Player TestPlayer = new Player(0);
+    private Player TestPlayer = new Player(1);
     private Tile TestTile = new Tile(game, 0,0,0,0, true, new Runnable() {
         @Override
         public void run() {
@@ -21,6 +22,29 @@ public class PlayerTest extends TesterFile {
         }
     });
     private Roboticon TestRoboticon = new Roboticon(0, TestPlayer, TestTile);
+    private College TestCollege = new College (1, "I am a test college.");
+
+    @Test
+    public void testAssignCollege(){
+        TestPlayer.assignCollege(TestCollege);
+        assertEquals(TestPlayer.getCollege(),TestCollege);
+    }
+
+    @Test
+    public void testAddRoboticon() {
+        Integer count = TestPlayer.getRoboticonCount();
+        count += 1;
+        TestPlayer.addRoboticon(TestRoboticon);
+        assertEquals(count, TestPlayer.getRoboticonCount());
+    }
+
+    @Test
+    public void testAssignTile() {
+        List<Tile> TileList = new ArrayList<Tile>();
+        assertEquals(TestPlayer.getTileList(),TileList);
+        TestPlayer.assignTile(TestTile);
+        assertNotEquals(TestPlayer.getTileList(), TileList);
+    }
 
     @Test
     public void ValidVaryResource() {
